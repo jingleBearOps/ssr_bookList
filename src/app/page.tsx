008e62ef60141a644e6e874e9dca60ba51633322
jingleBearOps <jingzhuot@gmail.com>
2023-11-19 11:33:50 -0800

    use client init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Home() {
+
+  return (
+    <main className="min-h-screen p-24">
+      <h2 className={'text-2xl font-bold text-center'}>Book Store</h2>
+    </main>
+  )
+}
