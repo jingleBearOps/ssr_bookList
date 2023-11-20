9e94e7b2f7a1d527eda39731f164940a75ad53f1
jingleBearOps <jingzhuot@gmail.com>
2023-11-19 22:03:18 -0800

    redux setup successfully

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {useDispatch} from "react-redux";
-
+import bookReducer from './book.slice'
 
 export const store = configureStore({
     // root reducer
     reducer: {
         // reducers
+        book: bookReducer
     }
 });
 
