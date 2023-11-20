abad75f1a6aa2518f3a3bd22eb2e0edbc810acdd
jingleBearOps <jingzhuot@gmail.com>
2023-11-20 11:38:12 -0800

    css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ReduxProvider from "./../store/ReduxProvider";
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Next.js',
@@ -14,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+       <ReduxProvider>
+           {children}
+       </ReduxProvider>
+      </body>
     </html>
   )
 }
